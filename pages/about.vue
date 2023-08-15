3bbdac1754e2ee69c23dd7df01337de8e2e1deaa
MadFury94 <onochieazukaeme@gmail.com>
2023-08-15 23:43:16 +0100

    about page 10%

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,12 @@
 <template>
-  <div></div>
+  <div>
+    <AboutHero />
+    hello
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import AboutHero from "./sections/AboutHero.vue";
+</script>
 
 <style lang="scss" scoped></style>
